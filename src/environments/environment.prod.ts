f1c4567438e5233ec50d5e9715b64e4b3e8f1b7a
unknown <evgenysamohvalov1988@gmail.com>
2020-07-18 09:37:24 +0300

    init (init learning progect)

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCfvrDCDUij0iT2JzbU3yiLVNN4Mp_pAAA',
+  fbDbUrl: 'https://angular-blog-e513b.firebaseio.com'
+};
